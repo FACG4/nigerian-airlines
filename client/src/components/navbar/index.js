ab96665c54bd5e40818e5ca11bc55f54a091b11a
razan99 <rozamohamed9548@gmail.com>
2018-07-26 14:19:42 +0300

    navbar

---
+++ client/src/components/navbar/index.js
@@ -0,0 +1,69 @@
+import React ,{Component}from 'react';
+import ReactDOM from 'react-dom';
+import image from  './username.png';
+import plan from  './plan.png';
+import img from  './newTable.png';
+import photo from  './tables.png';
+
+import './style.css'
+
+class Navbar extends Component{
+    constructor() {
+        super();
+        this.state={
+            activeTab: 'flights'
+        }
+
+        this.handleTabChange = this.handleTabChange.bind(this);
+    }
+
+    handleTabChange(activeTab) {
+        this.setState({
+            ...this.state,
+            activeTab
+        })
+    }
+
+    render(){
+        return(
+      <div>
+        <div className = "nav">
+            <div className ="user">
+            <a className="username" href={'#'} >username</a>
+                <img src={image} />
+                
+            </div>
+            <div>
+            <img className ="plane" src={plan} />
+            </div>
+        </div>
+        <div className="sideDiv">
+        <ul>
+            <li
+                onClick={() => this.handleTabChange('flights')}
+                className={this.state.activeTab === 'flights' ? 'active' : ''}
+            >
+            <br></br>
+           
+            <li className="fli">
+                <img className ="add"  src= {photo} />
+                <a  href={'#'} >Flights</a>
+                </li>
+                
+            </li>
+            <li
+                // onClick={() => this.handleTabChange('addflights')}
+                className={this.state.activeTab === 'addflights' ? 'active' : ''}
+            >
+                <img className ="addf"  src= {img}/>
+                <a href={'#'} >AddFlights</a>
+            </li>
+        </ul>
+        </div>
+      </div>
+
+        )
+
+    }
+}
+export default Navbar;
