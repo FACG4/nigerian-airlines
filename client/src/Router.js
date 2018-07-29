06bcb037c93b898e696ba8db72aada997fa9d896
InassTubail <anoos.hanii@gmail.com>
2018-07-29 21:34:22 +0300

    complete route

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import { Switch, Route ,BrowserRouter } from 'react-router-dom'
-import App from './App';
+import { Switch, Route ,BrowserRouter } from 'react-router-dom';
 import Login from './pages/login';
 import AddFlight from './pages/addFlight';
 import UpdateFlight from './pages/updateFlight';
 
-const Router = () =>(
-    <BrowserRouter>
-        <Switch>
-         <Route exact path="/" render={({history}) => (
-             <Login username="inass"/>
-         )} />
-         <Route path="/addflight" component={AddFlight} />
-         <Route path="/updateflight" component={UpdateFlight} />
-        </Switch>
-    </BrowserRouter>
-)
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' render={({ history }) => (
+        <Login username='inass'/>
+      )} />
+      <Route path='/addflight' component={AddFlight} />
+      <Route path='/updateflight' component={UpdateFlight} />
+    </Switch>
+  </BrowserRouter>
+);
 export default Router;
--- client/src/pages/addFlight/index.js
+++ client/src/pages/addFlight/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-import { DurationCard, Select, Input, TimeButton, Button, Header, SideNav} from '../../components'
+import { DurationCard, Select, Input, TimeButton, Button, Header, SideNav } from '../../components';
 import './addflight.css';
 
 class UpdateFlight extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      selectValue: '',
+      selectValue: ''
     };
 
     this.handleSelectChange = this.handleSelectChange.bind(this);
@@ -16,31 +16,31 @@ class UpdateFlight extends Component {
 
   handleSelectChange(value) {
     console.log(value);
-    this.setState({  selectValue: value });
+    this.setState({ selectValue: value });
   }
   render() {
     return (
-        <div className="addflight-container">
-          <Header />
-          <div className="container-content">
-            <div className="sub-container-sidenav-form">
-              <SideNav />
-              <div className="sub-container-form">
-                <Select labelText='Origin' firstItem='from' cities={['city1','city2']} onSelectChange={this.handleSelectChange} />
-                <Select labelText='Destination' firstItem='to' cities={['city1','city2']} onSelectChange={this.handleSelectChange} />
-                <Input labelText='Flight date' placeholder='flight date' />
-                <Input labelText='Aircraft type' placeholder='aircraft type' />
-                <div className='width-div'>
-                  <TimeButton textvalue='Calculate time' />
-                  <DurationCard flightduration='00 hours'/>
-                </div>
-                <div className='center-btn'>
-                  <Button textvalue='Add'/>
-                </div>
+      <div className='addflight-container'>
+        <Header />
+        <div className='container-content'>
+          <div className='sub-container-sidenav-form'>
+            <SideNav />
+            <div className='sub-container-form'>
+              <Select labelText='Origin' firstItem='from' cities={['city1','city2']} onSelectChange={this.handleSelectChange} />
+              <Select labelText='Destination' firstItem='to' cities={['city1','city2']} onSelectChange={this.handleSelectChange} />
+              <Input labelText='Flight date' placeholder='flight date' />
+              <Input labelText='Aircraft type' placeholder='aircraft type' />
+              <div className='width-div'>
+                <TimeButton textvalue='Calculate time' />
+                <DurationCard flightduration='00 hours'/>
+              </div>
+              <div className='center-btn'>
+                <Button textvalue='Add'/>
               </div>
             </div>
           </div>
         </div>
+      </div>
     );
   }
 }
