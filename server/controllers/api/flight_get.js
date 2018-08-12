e845dcfb2810a560eca969e3f642b103ca0b809d
amusameh <abdalsamad.y.m@gmail.com>
2018-08-12 09:37:30 +0300

    add catch in flight_get api handler

--- server/controllers/api/flight_get.js
+++ server/controllers/api/flight_get.js
@@ -5,5 +5,7 @@ export default (req, res, next) => {
     const flights = [];
     result.map(flight => flights.push(flight));
     res.send(JSON.stringify({ data: flights }));
+  }).catch((e) => {
+    res.status(500).send({ msg: 'Internal server error', erorr: e });
   });
 };
