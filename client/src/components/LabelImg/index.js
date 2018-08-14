e1d494218fc474ef2feaee98fb7c615b0dc22f54
amusameh <abdalsamad.y.m@gmail.com>
2018-08-14 10:43:24 +0300

    requested changes

--- client/src/components/LabelImg/index.js
+++ client/src/components/LabelImg/index.js
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 
 import "./labelimg.css";
 
-const LabelImg = ({ ImgClassName, labelText, imgSrc }) => (
-  <div className="labelimg-style">
+const LabelImg = ({ ImgClassName, labelText, imgSrc, ...rest }) => (
+  <div className="labelimg-style" {...rest}>
     <img src={imgSrc} className={ImgClassName} alt="img" />
     <label className="label-for-img-style">{labelText}</label>
   </div>
