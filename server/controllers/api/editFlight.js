822ae7aea9a94b3dc1755f7eeaaa7cc92ef022a0
InassTubail <anoos.hanii@gmail.com>
2018-08-15 09:57:28 +0300

    edit update flight

--- server/controllers/api/editFlight.js
+++ server/controllers/api/editFlight.js
@@ -4,7 +4,7 @@ export default(req, res, next) => {
   const {
     gateNo, status, terminalNo, departureTime,
   } = req.body;
-  const { flightId: flightNo } = req.params.flightId;
+  const { flightId: flightNo } = req.params;
 
   models.Flights
     .update({
